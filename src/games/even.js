cbaa2899eae667160134493fee12c85e95088615
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2022-12-04 13:58:00 +0300

    refact: src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,41 +1,23 @@
-import readlineSync from 'readline-sync';
+import { getRandomInt, runGame } from '../index.js';
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+const generateQuestion = () => {
+  const maxValue = 100;
+  const num = getRandomInt(maxValue);
+
+  let correctAnswer;
+  if (num % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+  const question = `Question: ${num}!`;
+  
+  return [question, correctAnswer];
+};
 
 const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const tries = 3;
-  for (let i = 0; i < tries; i += 1) {
-    const num = getRandomInt(100);
-
-    let correctAnswer;
-    if (num % 2 === 0) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-
-    console.log(`Question: ${num}!`);
-    const userInput = readlineSync.question('Your answer: ');
-
-    if (userInput !== correctAnswer) {
-      console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-
-    console.log('Correct!');
-
-    if (i === (tries - 1)) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+  runGame(rules, generateQuestion);
 };
 
 export default evenGame;
