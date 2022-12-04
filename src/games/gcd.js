2c22db9c085f0f121cecad7e989f36c6d893c3aa
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2022-12-04 15:19:27 +0300

    refact: src/games/gcd.js (linter error fix)

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,7 +7,7 @@ const generateQuestion = () => {
   const minValue = Math.min(num1, num2);
 
   let gcd = 1;
-  for (let i=1; i < minValue; i += 1) {
+  for (let i = 1; i < minValue; i += 1) {
     if ((num1 % i === 0) && (num2 % i === 0)) {
       gcd = i;
     }
